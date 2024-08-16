89e42d1562bb19b6ab42528be54ca5b8ac6b0db9
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-08-16 21:08:52 +0100

    Search Engine with tools and agents

---
+++ section 32/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun, WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+
+load_dotenv()
+
+arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+wiki_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+wiki = WikipediaQueryRun(api_wrapper=wiki_wrapper)
+
+search = DuckDuckGoSearchRun(name="Search")
+
+st.title("🔎 LangChain - Chat with search")
+"""
+    In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+    Try more Langchain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+"""
+
+st.sidebar.title("Settings")
+api_key = st.sidebar.text_input("Enter your Groq API key", type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[
+        {"role": "assistant", "content":"Hi, I'am a chatbot who can search the web. How can I help you?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if prompt:=st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    llm = ChatGroq(groq_api_key=api_key, model_name="Llama3-8b-8192")
+    tools = [search, arxiv, wiki]
+
+    search_agent = initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({'role':'assistant', 'content':response})
+        st.write(response)
